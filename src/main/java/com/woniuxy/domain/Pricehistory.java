849ae2589eb555f17c9a2554838905bc83b228d2
liuguizhuang <1203574679@qq.com>
2019-09-11 21:26:35 +0800

    Goodshistory和Pricehistory使用小辣椒修改

--- src/main/java/com/woniuxy/domain/Pricehistory.java
+++ src/main/java/com/woniuxy/domain/Pricehistory.java
@@ -2,6 +2,9 @@ package com.woniuxy.domain;
 
 import java.util.Date;
 
+import lombok.Data;
+
+@Data
 public class Pricehistory {
     private Integer phid;
 
@@ -12,44 +15,4 @@ public class Pricehistory {
     private Double phmoney;
 
     private Date phtime;
-
-    public Integer getPhid() {
-        return phid;
-    }
-
-    public void setPhid(Integer phid) {
-        this.phid = phid;
-    }
-
-    public Integer getGid() {
-        return gid;
-    }
-
-    public void setGid(Integer gid) {
-        this.gid = gid;
-    }
-
-    public Integer getUid() {
-        return uid;
-    }
-
-    public void setUid(Integer uid) {
-        this.uid = uid;
-    }
-
-    public Double getPhmoney() {
-        return phmoney;
-    }
-
-    public void setPhmoney(Double phmoney) {
-        this.phmoney = phmoney;
-    }
-
-    public Date getPhtime() {
-        return phtime;
-    }
-
-    public void setPhtime(Date phtime) {
-        this.phtime = phtime;
-    }
 }
