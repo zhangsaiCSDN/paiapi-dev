82e071ab5c66071e6ac13c20b90030f827b2d992
Xiaoxi <403411092@qq.com>
2019-09-12 11:37:00 +0800

    permission CRUD

---
+++ src/main/java/com/woniuxy/service/impl/IPermissionService.java
@@ -0,0 +1,13 @@
+package com.woniuxy.service.impl;
+
+import java.util.List;
+
+import com.woniuxy.domain.Permission;
+
+public interface IPermissionService {
+	void save(Permission p);
+	void delete(Integer pmsid);
+	void update(Permission p);
+	Permission findOne(Integer pmsid);
+	List<Permission> find();
+}
---
+++ src/main/java/com/woniuxy/service/impl/PermissionServiceImpl.java
@@ -0,0 +1,43 @@
+package com.woniuxy.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.woniuxy.dao.PermissionMapper;
+import com.woniuxy.domain.Permission;
+@Service
+@Transactional
+public class PermissionServiceImpl implements IPermissionService{
+
+	@Autowired
+	private PermissionMapper mapper; 
+	
+	@Override
+	public void save(Permission p) {
+		mapper.insert(p);
+	}
+
+	@Override
+	public void delete(Integer pmsid) {
+		mapper.deleteByPrimaryKey(pmsid);
+	}
+
+	@Override
+	public void update(Permission p) {
+		mapper.updateByPrimaryKeySelective(p);
+	}
+
+	@Override
+	public Permission findOne(Integer pmsid) {
+		return mapper.selectByPrimaryKey(pmsid);
+	}
+
+	@Override
+	public List<Permission> find() {
+		return mapper.selectByExample(null);
+	}
+	
+}
