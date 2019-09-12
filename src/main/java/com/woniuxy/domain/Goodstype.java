91b52b5a6ae4731ee84d39eb9087d94c46a7498d
lyx <77492124@qq.com>
2019-09-12 11:13:05 +0800

    2019年9月12日11:12:30 lyx添加service 和 domain 序列化

--- src/main/java/com/woniuxy/domain/Goodstype.java
+++ src/main/java/com/woniuxy/domain/Goodstype.java
@@ -1,9 +1,11 @@
 package com.woniuxy.domain;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public class Goodstype {
+public class Goodstype implements Serializable{
     private Integer gtid;
 
     private String gtname;
