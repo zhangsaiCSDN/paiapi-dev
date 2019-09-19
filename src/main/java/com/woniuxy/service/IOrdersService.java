94d82d6b4ebea6237a9d89638fd4142fc95a7dcc
lyr <asss@qq.com>
2019-09-19 09:16:40 +0800

    order分页成功

--- src/main/java/com/woniuxy/service/IOrdersService.java
+++ src/main/java/com/woniuxy/service/IOrdersService.java
@@ -3,6 +3,7 @@ package com.woniuxy.service;
 import java.util.List;
 
 import com.woniuxy.domain.Orders;
+import com.woniuxy.domain.Page;
 
 public interface IOrdersService {
 
@@ -10,5 +11,6 @@ public interface IOrdersService {
 	void delete(Integer odid);
 	void update(Orders orders);
 	Orders findOne(Integer odid);
-	List<Orders> find();
+	List<Orders> find(Page<Orders> page);
+	Integer count();
 }
--- src/main/java/com/woniuxy/web/controller/OrdersController.java
+++ src/main/java/com/woniuxy/web/controller/OrdersController.java
@@ -9,11 +9,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.woniuxy.domain.Deposit;
 import com.woniuxy.domain.Orders;
+import com.woniuxy.domain.Page;
 import com.woniuxy.domain.User;
+import com.woniuxy.service.IOrdersService;
 import com.woniuxy.service.impl.OrdersServiceImpl;
 
 @Controller
@@ -21,7 +25,7 @@ import com.woniuxy.service.impl.OrdersServiceImpl;
 public class OrdersController {
 	
 	@Autowired
-	private OrdersServiceImpl service;
+	private IOrdersService service;
 	
 	
 	@ResponseBody
@@ -40,20 +44,24 @@ public class OrdersController {
 	
 	@PutMapping
 	@ResponseBody
-	public void update(Orders orders) {
+	public void update(@RequestBody Orders orders) {
+		System.out.println("OrdersController.update()-------------");
 		service.update(orders);
 	}
 	
 	@GetMapping
 	@ResponseBody
-	public List<Orders> find(){
+	public Page<Orders> find(Integer p){
 		System.out.println(111);
-		List<Orders> find = service.find();
-		System.out.println(find);
-		for (Orders orders : find) {
+		if(p==null)p=1;
+		int count=service.count();
+		Page<Orders> page=new Page<>(p,count,5);
+		List<Orders> list=service.find(page);
+		page.setList(list);
+		for (Orders orders : list) {
 			System.out.println(orders);
-		} 
-		return find;
+		}
+		return page;
 	}
 	
 	@GetMapping(value = "/{odid}")
